f79a8a34a80812308052f731e747d1d82a4e179a
adalid969 <169183046+adalid969@users.noreply.github.com>
2024-08-06 13:53:56 -0400

    Add files via upload

---
+++ HTML Final Review/JS/custom-js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { //addressing head , header, footer.
+    $(function () {
+        $("#head").load("meta.html");
+        $("#header").load("header.html");
+        $("#footer").load("footer.html");
+
+    });
+});
